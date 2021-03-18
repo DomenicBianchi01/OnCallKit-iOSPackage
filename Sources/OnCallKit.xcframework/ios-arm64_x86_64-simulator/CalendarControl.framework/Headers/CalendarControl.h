6dde1b4798daa37cd338898b822614983a1b0f3b
Domenic Bianchi <domenic@oncallhealth.com>
2021-03-18 15:13:48 -0400

    Save

---
+++ Sources/OnCallKit.xcframework/ios-arm64_x86_64-simulator/CalendarControl.framework/Headers/CalendarControl.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:abb8ee13f45a9db0bd6d1f46ec2b05e2bb959305e7ed3cb9c8dd6207170a7d6d
+size 500
---
+++ Sources/OnCallKit.xcframework/ios-x86_64-maccatalyst/CalendarControl.framework/Versions/A/Headers/CalendarControl-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d21b00c786b5ad57ac3ffa5c8398653706542f4aa56d376731be9c8648924022
+size 9174
